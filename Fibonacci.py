afdafc32cd78d338e93d6467b360e587bae2e9f8
omarleon2000 <ruvm2021@gmail.com>
2025-08-28 16:40:47 -0400

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,10 +1,1 @@
-while True:
-    limite = int(input("Digite um número limte: "))
-    a, b = 0, 1
-    while a <= limite:
-        print(a, end=" ")
-        a, b = b, a + b
-    print()
-    continuar = input("Desea continuar? (s/n)")
-    if continuar != "s":
-        break
+
